5fc13965dc300e4faad27e5725c03cf53bf11f6d
BanditCook <tanguy.lalexandre@gmail.com>
2023-11-29 12:35:27 +0000

    Remove the credentials for email from settings

--- HugoMarquisCoaching/settings.py
+++ HugoMarquisCoaching/settings.py
@@ -171,8 +171,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hugomarquiswebsite@gmail.com'
-EMAIL_HOST_PASSWORD = 'ssub llyt wnym yemd'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 STATIC_URL = '/static/'
 
